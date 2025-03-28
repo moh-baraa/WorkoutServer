fee441895b4768e059e752e025842e5b1e48d12a
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-03-29 00:13:12 +0300

    workouts.js

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,23 +1,20 @@
- constexpress = require('express')
- constrouter =express.Router()
+const express = require('express')
+ const {
+ getWorkouts, 
+getWorkout, 
+createWorkout, 
+deleteWorkout, 
+updateWorkout
+ } = require('../controllers/workoutController')
+ const router = express.Router()
  // GET all workouts
- router.get('/', (req, res) => {
- res.json({mssg: 'GET all workouts'})
- })
+ router.get('/', getWorkouts)
  // GET a single workout
- router.get('/:id', (req, res) =>{
- res.json({mssg: 'GET a single workout'})
- })
+ router.get('/:id', getWorkout)
  // POST a new workout
- router.post('/', (req, res) => {
- res.json({mssg: 'POST a new workout'})
- })
+ router.post('/', createWorkout)
  // DELETE a workout
- router.delete('/:id', (req, res) => {
- res.json({mssg: 'DELETE a workout'})
- })
+ router.delete('/:id', deleteWorkout)
  // UPDATE a workout
- router.patch('/:id', (req, res) => {
- res.json({mssg: 'UPDATE a workout'})
- })
- module.exports= router
+ router.patch('/:id', updateWorkout)
+ module.exports = router
