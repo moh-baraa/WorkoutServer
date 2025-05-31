08b9a0e6882f41eb0bfe48e87f27895d093f15b5
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-05-31 05:12:50 +0300

    workoutModels.js

--- models/workoutModels.js
+++ models/workoutModels.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose')
- const workoutSchema = new mongoose.Schema({
- title: {
- type: String,
- required: true
- },
- reps: {
- type: Number,
- required: true
- },
- load: {
- type: Number,
- required: true
- }
- }, { timestamps: true })
- module.exports 
+const workoutSchema = new mongoose.Schema({
+    title: { //workout name
+        type: String,
+        required: true
+    },
+    reps: { // how many time u did the workout
+        type: Number,
+        required: true
+    },
+    load: { // how much weight 
+        type: Number,
+        required: true
+    },
+    user_id:{
+        type:String,
+        required:true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Workout', workoutSchema)
