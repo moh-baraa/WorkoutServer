45eb8a23870cecc7834a45a047ad3a32f6feef5a
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-05-31 05:11:21 +0300

    workouts.js

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,20 +1,28 @@
 const express = require('express')
- const {
- getWorkouts, 
-getWorkout, 
-createWorkout, 
-deleteWorkout, 
-updateWorkout
- } = require('../controllers/workoutController')
- const router = express.Router()
- // GET all workouts
- router.get('/', getWorkouts)
- // GET a single workout
- router.get('/:id', getWorkout)
- // POST a new workout
- router.post('/', createWorkout)
- // DELETE a workout
- router.delete('/:id', deleteWorkout)
- // UPDATE a workout
- router.patch('/:id', updateWorkout)
- module.exports = router
+const router = express.Router()
+//const Workout = require('../models/workoutModels')
+//const mongoose = require('mongoose')
+const {
+    getWorkout,
+    getWorkouts,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController.js')
+const requireAuth = require('../middleware/requireAuth.js')
+//require auth for all workout routes
+router.use(requireAuth)
+
+//Get all workouts
+router.get('/', getWorkouts)
+
+// Get a single workout
+router.get('/:id', getWorkout)
+
+router.post('/', createWorkout)
+
+router.delete('/:id', deleteWorkout)
+
+router.patch('/:id', updateWorkout)
+
+module.exports = router
